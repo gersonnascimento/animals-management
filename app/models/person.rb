cec1ac8ec56afecbef50058173aa1b27693b6330
Gerson <gerson.nascimento@locaweb.com.br>
2019-07-24 19:40:49 -0300

    Created calculating age method and added rspec, factory-bot and pry gems.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,7 @@
 class Person < ApplicationRecord
   has_many :animals
+
+  def age
+    ((Time.now - birth_date.to_time) / 1.year.seconds).floor
+  end
 end
---
+++ spec/models/person_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Person, type: :model do
+  context 'When calculating age of a person with exactly 10 years old' do
+    it 'should return age correctly' do
+      person = build(:person, birth_date: 10.years.ago)
+
+      expect(person.age).to eq(10)
+    end
+  end
+
+  context 'When calculating age of a person with almost 18 years old' do
+    it 'should return 17 years old' do
+      person = build(:person, birth_date: 18.years.ago.next_month)
+
+      expect(person.age).to eq(17)
+    end
+  end
+end
