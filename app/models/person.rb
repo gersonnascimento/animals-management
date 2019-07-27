2929a07c70eb36b0e5471a5fde55b6a2d3772c51
Gerson <gerson.nascimento@locaweb.com.br>
2019-07-27 03:04:46 -0300

    fix age calculating

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,13 @@ class Person < ApplicationRecord
   has_many :animals
 
   def age
-    ((Time.now - birth_date.to_time) / 1.year.seconds).floor
+    now = Time.zone.now
+    birth = birth_date.to_time
+    person_age = now.year - birth.year
+    unless now.month == birth.month && now.day >= birth.day || now.month > birth.month
+      person_age = person_age.pred
+    end
+    person_age
   end
 
   def monthly_cost
